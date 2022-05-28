2bd52c4dd0fc82e988f131c6acbb6e19c535ab4d
Moeed <63580699+MoeedthePessimist@users.noreply.github.com>
2022-05-29 00:03:46 +0500

    removing jade templating

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ var headRouter = require("./routes/head");
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// // view engine setup
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -52,13 +52,7 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
-
-	// render the error page
-	res.status(err.status || 500);
-	res.render("error");
+	res.send(err);
 });
 
 module.exports = app;
--- routes/teacher.js
+++ routes/teacher.js
@@ -12,7 +12,7 @@ module.exports = router;
 router.post("/addQuiz", async (req, res, next) => {
 	try {
 		const quiz = new Quiz(req.body.quiz);
-		console.log(quiz);
+		// console.log(quiz);
 		const added = await quiz.save();
 		res.json(added);
 	} catch (error) {
