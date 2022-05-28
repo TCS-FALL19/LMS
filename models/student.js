eabc07401ca356d12843d0c6d880bb11c6d7c25e
Abdullah <abdullahrecon999@gmail.com>
2022-05-28 14:39:17 +0500

    Initial Admin Routes

---
+++ models/student.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var studentSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    rollno:{
+        type:String,
+        required:true
+    }
+});
+
+module.exports = mongoose.model('Student',studentSchema);
