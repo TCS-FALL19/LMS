73f358370e7ebee2fc16b716a1da99d209756680
Muhammadharis9583 <m.haris8790@gmail.com>
2022-05-29 17:32:40 +0500

    Added Student Update Contact

--- routes/student.js
+++ routes/student.js
@@ -4,6 +4,7 @@ const multer = require("multer");
 var router = express.Router();
 
 const Assignment = require("../models/assignment");
+const Student = require("../models/student")
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -37,6 +38,21 @@ router.put("/submitAssignment", upload.single("AttachedFile"), (req, res) => {
           marks: 0,
         },
       },
+    },
+    { new: true, upsert: false },
+    function (err, result) {
+      if (err) {
+        return next(error);
+      }
+      res.json(result);
+    }
+  );
+});
+router.put("/updateContact", (req, res) => {
+  Student.findOneAndUpdate(
+    { _id: req.body._id },
+    {
+      contact: req.body.contact
     },
     { new: true, upsert: false },
     function (err, result) {
