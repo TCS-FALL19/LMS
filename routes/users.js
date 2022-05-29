8dcf71434131a4ef43680b5732608ee13dc8607f
manalkiyani <manalkiyani687@gmail.com>
2022-05-29 13:55:30 -0700

    logout

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+app.get('/logout',authenticateToken,async (req,res)=>
+{
+try{
 
+    res.clearCookie("token").status(200).send("logout successful");
+}
+catch{
+    res.status(500).send();
+}
+});
 module.exports = router;
