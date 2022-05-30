9141be0d0ffe38556ab72ea7d3e4316e3a12eb72
Muhammad Awais Awan <awaixsawan@gmail.com>
2022-05-30 17:55:19 +0500

    C:/Program Files/Git/profile route added for Student to view profile (assignment 2)

--- routes/student.js
+++ routes/student.js
@@ -1,8 +1,24 @@
 const express = require("express");
 var router = express.Router();
+var Student = require("../models/student");
 
 router.get("/", (req, res, next) => {
 	res.send("Student Dashboard");
 });
+router.get("/profile", (req, res, next) => {
+	Teacher.findById(req.params.id)
+	.then(
+		(student) => {
+			res.statusCode = 200;
+			res.setHeader("Content-Type", "application/json");
+			res.json(student);
+		},
+		(err) => next(err)
+	)
+	.catch((err) => next(err)); 
+
+	res.send("Student view profile");
+});
+
 
 module.exports = router;
