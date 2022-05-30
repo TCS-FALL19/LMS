0a67e6e754573e0963c63896d1446b76f30cb53d
Moeed <63580699+MoeedthePessimist@users.noreply.github.com>
2022-05-30 23:55:43 +0500

    Update index.js
    putting the routes in their destined files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.json("index", { title: "Express" });
-});
+//..................................................
+//Implemented by Hafeez Ullah
+//..................................................
+
+router.get('/', function(req, res){
+  res.send('Welcome to LMS');
+})
+
+
+//..................................................
+//Implemented by Hamza Riaz Khan
+//..................................................
+
+router.get('/error', function(req, res){
+  res.send('This is the Error Page!');
+})
 
 module.exports = router;
