c5d98d1f2bd39c191c8676f5c1fe61fd32022260
Moeed <63580699+MoeedthePessimist@users.noreply.github.com>
2022-05-30 23:48:49 +0500

    Delete error.js

--- routes/error.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-/* GET Error Route. */
-router.get("/error", function (req, res, next) {
-  res.json("This is the Error Page!", { title: "Express" });
-});
-
-module.exports = router;
