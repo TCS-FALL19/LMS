d954873919466e35b363ceff56ec0346b94e8c99
Moeed <63580699+MoeedthePessimist@users.noreply.github.com>
2022-05-30 16:11:53 +0500

    getMarks route for student completed

--- routes/student.js
+++ routes/student.js
@@ -5,7 +5,7 @@ const Quiz = require("../models/quiz");
 var router = express.Router();
 
 const Assignment = require("../models/assignment");
-const Student = require("../models/student")
+const Student = require("../models/student");
 
 var storage = multer.diskStorage({
 	destination: function (req, file, cb) {
@@ -19,18 +19,17 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage });
 
 router.get("/", (req, res, next) => {
-  console.log(req.body.answer)
-  res.send("Student Dashboard");
+	console.log(req.body.answer);
+	res.send("Student Dashboard");
 });
 
 router.get("/:sid", (req, res, next) => {
-	Student.findById(req.params.sid).exec((err,result) => {
-		if (err)
-		{
+	Student.findById(req.params.sid).exec((err, result) => {
+		if (err) {
 			return next(err);
 		}
 		res.json(result);
-	})
+	});
 });
 
 router.put(
@@ -65,38 +64,72 @@ router.put(
 );
 
 router.put("/updateContact", (req, res) => {
-  Student.findOneAndUpdate(
-    { _id: req.body._id },
-    {
-      contact: req.body.contact
-    },
-    { new: true, upsert: false },
-    function (err, result) {
-      if (err) {
-        return next(error);
-      }
-      res.json(result);
-    }
-  );
+	Student.findOneAndUpdate(
+		{ _id: req.body._id },
+		{
+			contact: req.body.contact,
+		},
+		{ new: true, upsert: false },
+		function (err, result) {
+			if (err) {
+				return next(error);
+			}
+			res.json(result);
+		}
+	);
 });
 
-
 router.put("/attemptquiz/:qID/:sID", async (req, res, next) => {
-  Quiz.findOneAndUpdate({ _id: req.params.qID }, {
-      "$push": {
-        "submissions": {
-            "student": req.params.sID,
-            "answers":req.body.answer,
-            "marks":0
+	Quiz.findOneAndUpdate(
+		{ _id: req.params.qID },
+		{
+			$push: {
+				submissions: {
+					student: req.params.sID,
+					answers: req.body.answer,
+					marks: 0,
+				},
+			},
+		},
+		{ new: true, upsert: false },
+		function (error, results) {
+			if (error) {
+				return next(error);
+			}
+			res.json(results);
 		}
-      }
-    }, { new: true, upsert: false },
-    function(error, results) {
-        if (error) {
-            return next(error);
-        }
-        res.json(results);
-    });
+	);
+});
+
+// view both quiz and assignment marks for a specific subject
+router.get("/getMarks/:id", async (req, res, next) => {
+	const sID = req.params.id;
+	const subject = req.body.subject;
+	try {
+		const quiz = await Quiz.findOne({
+			subject: subject,
+			submissions: { $elemMatch: { student: sID } },
+		});
+		const quizAttainedMarks = quiz.submissions.find((submission) => {
+			return submission.student == sID;
+		}).marks;
+		const assignment = await Assignment.findOne({
+			student_submissions: { $elemMatch: { student_id: sID } },
+			subject: subject,
+		});
+		const assignmentAttainedMarks = assignment.student_submissions.find(
+			(submission) => {
+				return submission.student_id == sID;
+			}
+		).marks;
+		res.json({
+			qMarks: quizAttainedMarks,
+			aMarks: assignmentAttainedMarks,
+			subject: subject,
+		});
+	} catch (error) {
+		next(error);
+	}
 });
 
 module.exports = router;
